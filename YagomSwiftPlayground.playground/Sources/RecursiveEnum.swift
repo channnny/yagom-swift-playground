445d6af8e96b39de3eef88a56c9e0d40cf7c5676
Channy <uchanism24@naver.com>
2024-08-08 00:20:15 +0900

    update RecursiveEnum

--- YagomSwiftPlayground.playground/Sources/RecursiveEnum.swift
+++ YagomSwiftPlayground.playground/Sources/RecursiveEnum.swift
@@ -1,2 +1,27 @@
 import Foundation
 
+indirect enum ArithmeticExpression {
+    case number(Int)
+    case addition(ArithmeticExpression, ArithmeticExpression)
+    case multiplication(ArithmeticExpression, ArithmeticExpression)
+}
+
+let five = ArithmeticExpression.number(5)
+let four = ArithmeticExpression.number(4)
+let sum = ArithmeticExpression.addition(five, four)
+let product = ArithmeticExpression.multiplication(sum, ArithmeticExpression.number(2))
+
+func evaluate(_ expression: ArithmeticExpression) -> Int {
+    switch expression {
+    case let .number(value):
+        return value
+    case let .addition(left, right):
+        return evaluate(left) + evaluate(right)
+    case let .multiplication(left, right):
+        return evaluate(left) * evaluate(right)
+    }
+}
+
+public func printEvaluate() {
+    print(evaluate(product))
+}
